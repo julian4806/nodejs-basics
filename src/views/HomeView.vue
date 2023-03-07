6b4e7897c4eb55066541bc0e393aa5adc6accf74
Julian van Husen <210549@student.glu.nl>
2023-03-07 09:52:24 +0100

    added tailwind classes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld class="font-bold text-2xl" msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
